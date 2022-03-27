bd0be8bacb8c5baac191cc719fc3c97f9f2e074c
Patricia Sittikul <pas217@lehigh.edu>
2022-03-27 15:13:47 -0400

    Added lock file

---
+++ header.php
@@ -0,0 +1,8 @@
+
+<!doctype html>
+<html>
+    <head>
+        <title>2022 walmart PP gang reading challenge</title>
+        <link rel='stylesheet' href='assets/styles/styles.css'>
+    </head>
+</html>
