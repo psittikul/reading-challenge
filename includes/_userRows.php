293b69346617c9577ea8a898b0ef4b01d217c2f8
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 19:18:00 -0400

    Users

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -7,7 +7,7 @@
                 <img src='<?php echo $row['image_path'];?>'>
             </div>
             <div class='col-sm stats'>
-                <h4>Books Read: </h4>
+                <h4><i class="fa-solid fa-books"></i>Books Read: </h4>
             </div>
         </div>
 <?php
