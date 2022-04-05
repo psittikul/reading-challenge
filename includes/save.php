c030c770024058e58a84982fbcc1be1579746cca
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 22:24:18 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -1,8 +1,8 @@
 <?php
     // var_dump($_POST);
-    $query = "INSERT INTO books(title, author, status, date_read, user_id) VALUES(" . $_POST['title'] .
-        "," . $_POST['author'] . "," . $_POST['status'] . "," . $_POST['date_read'] . "," . $_POST['user_id'] . ")";
-    
+    // $query = "INSERT INTO books(title, author, status, date_read, user_id) VALUES(" . $_POST['title'] .
+    //     "," . $_POST['author'] . "," . $_POST['status'] . "," . $_POST['date_read'] . "," . $_POST['user_id'] . ")";
+    $query = "INSERT INTO books(title, user_id) VALUES('" . $_POST['title'] . "'," . $_POST['user_id'] . ")";
     $result = $conn->query($query);
 
     if(mysqli_error($conn)) {
