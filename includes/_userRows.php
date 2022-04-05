0a26c16dd3736f82ecb52f174d1a42b2b2b57a84
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 20:13:30 -0400

    Users

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -1,17 +1,16 @@
 <?php 
-    $result = $conn->query('SELECT name, image_path, count(books.id) AS bookCount FROM users LEFT OUTER JOIN books ON users.id = books.user_id GROUP BY users.id ORDER BY bookCount DESC');
+    $result = $conn->query('SELECT name, image_path, color, count(books.id) AS bookCount FROM users LEFT OUTER JOIN books ON users.id = books.user_id GROUP BY users.id ORDER BY bookCount DESC');
     while($row = $result->fetch_assoc()) {
 ?>
         <div class='row user-row'>
             <h2 class='user-name' style='background: <?php echo $row['color'];?>'>
                 <?php echo $row['name'];?>
             </h2>
-            <div class='col-sm-4 user-pic'>
+            <div class='col-sm-3 user-pic'>
                 <img src='<?php echo $row['image_path'];?>'>
             </div>
             <div class='col-sm stats'>
                 <h1>📚 <?php echo $row['bookCount'];?></h1>
-                <?php var_dump($row);?>
             </div>
         </div>
 <?php
