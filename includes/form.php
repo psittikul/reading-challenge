c6c05e1fd7c188e712005943696e5fc762d7f7fc
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 21:02:34 -0400

    Users

--- includes/form.php
+++ includes/form.php
@@ -6,15 +6,19 @@
     <div class="mb-3">
         <input type="text" class="form-control" data-column="author" placeholder="Author">
     </div>
-    <div class="mb-3 input-group">
-        <label>Status</label>
-        <select class="form-select" data-column="status">
-            <option selected value="Read">Read</option>
-            <option value="Currently Reading">Currently Reading</option>
-            <option value="To Be Read">To Be Read</option>
-        </select>
-        <label>Date Read</label>
-        <input type="date" class="form-control" data-column='date_read'>
+    <div class="mb-3 input-group row">
+        <div class='col-sm-6'>
+            <label>Status</label>
+            <select class="form-select" data-column="status">
+                <option selected value="Read">Read</option>
+                <option value="Currently Reading">Currently Reading</option>
+                <option value="To Be Read">To Be Read</option>
+            </select>
+        </div>
+        <div class='col-sm-6'>
+            <label>Date Read</label>
+            <input type="date" class="form-control" data-column='date_read'>
+        </div>
     </div>
     <button type="button" class="update-btn btn btn-primary" id="addBook<?php echo $row['userID'];?>">Save</button>
 </form>
