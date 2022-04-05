cd75cfa88155976dcc3cdfc24c30a20ed75d0779
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 20:07:21 -0400

    Users

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -4,13 +4,13 @@
 ?>
         <div class='row user-row'>
             <h2 class='user-name' style='background: <?php echo $row['color'];?>'>
-                <?php echo $row['name'];?>
+                <?php echo $row['name']; echo $row['color'];?>
             </h2>
             <div class='col-sm-2 user-pic'>
                 <img src='<?php echo $row['image_path'];?>'>
             </div>
             <div class='col-sm stats'>
-                <h4>📚 Books Read: <?php echo $row['bookCount'];?></h4>
+                <h4>📚 <?php echo $row['bookCount'];?></h4>
             </div>
         </div>
 <?php
--- includes/header.php
+++ includes/header.php
@@ -12,5 +12,5 @@
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class='container-sm'>
+        <div class='container-fluid'>
             <h1 id='pageTitle'>2022 WALMART PP GANG READING CHALLENGE</h1>
