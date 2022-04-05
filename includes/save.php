d745347cfc709730e03197d7a117af8b1f87c970
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 23:04:30 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -1,21 +1,11 @@
 <?php
-    include "connection.php";
-    // $query = "INSERT INTO books(title, user_id) VALUES('Alif the Unseen', " . $_POST['user_id'] . ");";
-    $query = "INSERT INTO books(id, title, user_id) VALUES(NULL, 'Alif the Unseen', 24);";
+    $query = "INSERT INTO books(id, title, author, user_id) VALUES(NULL, '" . $_POST['title'] . "', '" . $_POST['author'] . "', " . $_POST['user_id'] . ")";
     if($conn->query($query)) {
         echo json_encode(['data'=>$query, 'message'=>'SUCCESS????????']);
     }
     else {
         echo json_encode("Error: " . $query . "<br>" . $conn->error);
     }
-    // if($conn->query($query)) {
-    //     echo json_encode(['data'=>$query, 'msg'=>'HOORAY']);
-    // }
-
-    // else {
-    //     echo json_encode('idfk');
-    // }
-    // $query = "INSERT INTO books(title, author, user_id) VALUES('" . $_POST['title'] . "', '" . $_POST['author'] . "', " . $_POST['user_id'] . ")";
     // echo json_encode(['data'=>'help']);
     // $query = "INSERT INTO books(title, author, status, user_id) VALUES('" . $_POST['title'] .
         // "', '" . $_POST['author'] . "', '" . $_POST['status'] . "'," . $_POST['user_id'] . ")";
