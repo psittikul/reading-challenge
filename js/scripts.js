f6dd181be53889083c23d766cd18fa752fda3240
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 21:21:11 -0400

    Users

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $('.update-btn').on('click', function() {
-    var title = $(this).parent().find(["data-column='title'"]);
-    console.log('Update standings with this info: user ID=' + $(this).parent().data('user') + ' title: ' + title);
+    // var title = $(this).parent().find(["data-column='title'"]);
+    console.log('Update standings with this info: user ID=' + $(this).parent().data('user'));
     // $.ajax({
     //     method: "POST",
     //     url: "save.php",
