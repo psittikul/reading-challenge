ae82bb6c5b27e66a9fc0bb05caf2c6362a1c927f
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 22:40:52 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -3,7 +3,8 @@
         "," . $_POST['author'] . "," . $_POST['status'] . "," . $_POST['date_read'] . "," . $_POST['user_id'] . ")";
     // $query = "INSERT INTO books(title, user_id) VALUES('test', 24)";
     // $conn->query($query);
-    $result = $conn->query($query);
+    echo json_encode(['query'=>$query]);
+    // $result = $conn->query($query);
 
     if(mysqli_error($conn)) {
         echo json_encode(['data'=>"Error: $query " . mysqli_error($conn)]);
