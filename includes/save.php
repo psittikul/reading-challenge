116a092fded969e5232211e4d39bc1477c52a3ef
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 22:54:38 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -1,12 +1,13 @@
 <?php
     $query = "INSERT INTO books(title, user_id) VALUES('Alif the Unseen', " . $_POST['user_id'] . ");";
-    if($conn->query($query)) {
-        echo json_encode(['data'=>$query, 'msg'=>'HOORAY']);
-    }
+    echo json_encode(['data'=>$query, 'message'=>'maybe ID issue']);
+    // if($conn->query($query)) {
+    //     echo json_encode(['data'=>$query, 'msg'=>'HOORAY']);
+    // }
 
-    else {
-        echo json_encode('idfk');
-    }
+    // else {
+    //     echo json_encode('idfk');
+    // }
     // $query = "INSERT INTO books(title, author, user_id) VALUES('" . $_POST['title'] . "', '" . $_POST['author'] . "', " . $_POST['user_id'] . ")";
     // echo json_encode(['data'=>'help']);
     // $query = "INSERT INTO books(title, author, status, user_id) VALUES('" . $_POST['title'] .
