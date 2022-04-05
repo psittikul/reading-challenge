795c542221979e7aca6b2e660e6d53f2c1fa35bc
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 20:11:12 -0400

    Users

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -4,13 +4,14 @@
 ?>
         <div class='row user-row'>
             <h2 class='user-name' style='background: <?php echo $row['color'];?>'>
-                <?php echo $row['name']; echo $row['color'];?>
+                <?php echo $row['name'];?>
             </h2>
-            <div class='col-sm-2 user-pic'>
+            <div class='col-sm-4 user-pic'>
                 <img src='<?php echo $row['image_path'];?>'>
             </div>
             <div class='col-sm stats'>
-                <h4>📚 <?php echo $row['bookCount'];?></h4>
+                <h1>📚 <?php echo $row['bookCount'];?></h1>
+                <?php var_dump($row);?>
             </div>
         </div>
 <?php
