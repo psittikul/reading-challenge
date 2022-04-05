a28042850391cfa905eee3230d332936a151f8dd
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 22:40:04 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -1,17 +1,14 @@
 <?php
-    // $query = "INSERT INTO books(title, author, status, date_read, user_id) VALUES(" . $_POST['title'] .
-    //     "," . $_POST['author'] . "," . $_POST['status'] . "," . $_POST['date_read'] . "," . $_POST['user_id'] . ")";
-    $query = "INSERT INTO books(title, user_id) VALUES('" . $_POST['title'] . "'," . $_POST['user_id'] . ")";
-    echo json_encode(['data'=>$query]);
+    $query = "INSERT INTO books(title, author, status, date_read, user_id) VALUES(" . $_POST['title'] .
+        "," . $_POST['author'] . "," . $_POST['status'] . "," . $_POST['date_read'] . "," . $_POST['user_id'] . ")";
     // $query = "INSERT INTO books(title, user_id) VALUES('test', 24)";
     // $conn->query($query);
-    // $result = $conn->query($query);
+    $result = $conn->query($query);
 
-    // if(mysqli_error($conn)) {
-    //     echo "Error: $query " . mysqli_error($conn);
-    // }
+    if(mysqli_error($conn)) {
+        echo json_encode(['data'=>"Error: $query " . mysqli_error($conn)]);
+    }
 
-    // else {
-    //     // echo "Success!!";
-    //     var_dump($_POST);
-    // }
+    else {
+        echo json_encode(['query'=>$query]);
+    }
