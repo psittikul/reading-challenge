d52dd8c1827125fed239e7422e7569ae96d76d01
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 20:04:06 -0400

    Users

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -3,12 +3,14 @@
     while($row = $result->fetch_assoc()) {
 ?>
         <div class='row user-row'>
-            <h2 class='user-name'><?php echo $row['name'];?></h2>
+            <h2 class='user-name' style='background: <?php echo $row['color'];?>'>
+                <?php echo $row['name'];?>
+            </h2>
             <div class='col-sm-2 user-pic'>
                 <img src='<?php echo $row['image_path'];?>'>
             </div>
             <div class='col-sm stats'>
-                <h4><i class="fa-solid fa-books"></i>Books Read: <?php echo $row['bookCount'];?></h4>
+                <h4>📚 Books Read: <?php echo $row['bookCount'];?></h4>
             </div>
         </div>
 <?php
