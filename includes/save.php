2d6981132f22d5df8b7c0f9520371046a90e714a
Patricia Sittikul <pas217@lehigh.edu>
2022-04-04 22:45:31 -0400

    Users

--- includes/save.php
+++ includes/save.php
@@ -1,10 +1,11 @@
 <?php
-    $query = "INSERT INTO books(title, author, user_id) VALUES('" . $_POST['title'] . "', '" . $_POST['author'] . "', " . $_POST['user_id'] . ")";
+    // $query = "INSERT INTO books(title, author, user_id) VALUES('" . $_POST['title'] . "', '" . $_POST['author'] . "', " . $_POST['user_id'] . ")";
+    echo json_encode($_POST);
     // $query = "INSERT INTO books(title, author, status, user_id) VALUES('" . $_POST['title'] .
         // "', '" . $_POST['author'] . "', '" . $_POST['status'] . "'," . $_POST['user_id'] . ")";
     // $query = "INSERT INTO books(title, user_id) VALUES('test', 24)";
     // $conn->query($query);
-    echo json_encode(['query'=>$query]);
+    // echo json_encode(['query'=>$query]);
     // $result = $conn->query($query);
 
     if(mysqli_error($conn)) {
