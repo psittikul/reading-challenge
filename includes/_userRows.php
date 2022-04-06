4d70502ff8cf4c885603d40699954a40c28035b8
Patricia Sittikul <pas217@lehigh.edu>
2022-04-05 20:54:57 -0400

    Standings

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -1,10 +1,10 @@
 <?php 
-    $result = $conn->query('SELECT users.id as userID, name, image_path, color, count(books.id) AS bookCount FROM users LEFT OUTER JOIN books ON users.id = books.user_id where books.status = "Read" GROUP BY users.id ORDER BY bookCount DESC');
-    $standings = [];
+    $result = $conn->query('SELECT users.id as userID, name, image_path, color, count(books.id) AS bookCount FROM users LEFT OUTER JOIN books ON users.id = books.user_id where books.status = "Read" GROUP BY users.id ORDER BY bookCount DESC, name ASC');
+    // $standings = [];
     // $q1 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q1) on (books.user_id = users.id) group by users.id');
-    $q2 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q2) on (books.user_id = users.id) group by users.id');
-    $q3 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q3) on (books.user_id = users.id) group by users.id');
-    $q4 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q4) on (books.user_id = users.id) group by users.id');
+    // $q2 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q2) on (books.user_id = users.id) group by users.id');
+    // $q3 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q3) on (books.user_id = users.id) group by users.id');
+    // $q4 = $conn->query('select users.*, count(books.id) as bookCount from users left outer join books partition(q4) on (books.user_id = users.id) group by users.id');
    
     while($row = $result->fetch_assoc()) {
 ?>
@@ -17,15 +17,23 @@
             </div>
             <div class='col-sm stats'>
                 <div class='row'>
-                    <div class='col'>
+                    <div class='col-sm-3'>
                         <?php
                             $q1 = $conn->query('select count(books.id) as booksRead from books partition(q1) where books.user_id = ' . $row['userID']);
                         ?>
-                        <h1>📚 
+                        <h2>Q1 📚 
                             <?php 
                                 // echo $row['bookCount'];
-                                var_dump($q1->fetch_column(0));
-                            ?></h1>
+                                $q1->fetch_column(0);
+                            ?></h2>
+                    </div>
+                    <div class='col-sm-3'>
+                        <?php $q2 = $conn->query('select count(books.id) as booksRead from books partition(q2) where books.user_id = ' . $row['userID']);?>
+                        <h2>Q2 📚 
+                            <?php 
+                                // echo $row['bookCount'];
+                                $q2->fetch_column(0);
+                            ?></h2>
                     </div>
                 </div>
                 <div class='row'>
