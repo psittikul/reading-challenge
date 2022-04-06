3c7150937c02f6cf61c110e132dc81dea0e0f373
Patricia Sittikul <pas217@lehigh.edu>
2022-04-05 21:45:17 -0400

    Stats

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -61,7 +61,20 @@
                         <h4>Currently Reading: </h4>
                     </div>
                 </div>
-                <?php include "form.php";?>
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <?php include "form.php";?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 <?php
