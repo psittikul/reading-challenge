c2e19ab1a286489a2cf95b4eb4ca64e01f003ecd
Patricia Sittikul <pas217@lehigh.edu>
2022-04-10 17:48:49 -0400

    details page

--- details.php
+++ details.php
@@ -1,7 +1,8 @@
 <?php
-    include 'includes/connection.php';
+    // include 'includes/connection.php';
     include 'includes/header.php';
     include 'models/User.php';
+    include 'models/Prompt.php';
     // Page to list all books, like the original spreadsheet
 ?>
 <table class="table table-bordered" id="detailsTable">
@@ -16,8 +17,8 @@
   </thead>
   <tbody>
     <?php
-    $prompts = $conn->query('select * from prompts');
-    while($prompt = $prompts->fetch_assoc()) {
+    // $prompts = $conn->query('select * from prompts');
+    while($prompt = Prompt::all()) {
     ?>
     <tr class='prompt-row' data-id="<?php echo $prompt['id'];?>">
         <td>
--- models/Prompt.php
+++ models/Prompt.php
@@ -1,8 +1,13 @@
 <?php
 
-class Prompt {
+class Prompt extends Model {
 
     //Properties
     public $id;
     public $prompt;
+
+    public function all() {
+        $all = $this->DB->conn->query('SELECT * FROM prompts');
+        return $all->fetch_assoc();
+    }
 }
