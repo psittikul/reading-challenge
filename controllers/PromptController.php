21dd7eaddd9e506a899da121b366a59893595e55
Patricia Sittikul <pas217@lehigh.edu>
2022-04-10 18:23:44 -0400

    details page

--- controllers/PromptController.php
+++ controllers/PromptController.php
@@ -1,5 +1,6 @@
 <?php
 include '../models/Prompt.php';
+include '../models/User.php';
 class PromptController {
     private $model;
 
@@ -8,12 +9,17 @@ class PromptController {
     }
 
     public function list() {
-        echo "Get data needed";
+        echo "Get data needed\n";
         $Prompt = $this->model;
+        echo "Is ";
         $User = new User();
+        echo " this ";
         $conn = $this->model->DB->conn;
+        echo " thing ";
         $prompts = $Prompt->all();
+        echo " still ";
         $users = [];
+        echo " on?\n";
         while ($row = $conn->query("SELECT * FROM users ORDER BY users.order DESC")->fetch_assoc()) {
             $users[] = $User->find($row['id']);
         }
