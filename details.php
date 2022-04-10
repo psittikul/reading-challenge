ac98d63f9fc1854d08cf3ce9bfc14088d2f997d8
Patricia Sittikul <pas217@lehigh.edu>
2022-04-10 15:54:56 -0400

    Details page

---
+++ details.php
@@ -0,0 +1,6 @@
+<?php
+    include 'includes/connection.php';
+    include 'includes/header.php';
+    include 'includes/footer.php';
+    // Page to list all books, essentially like the Google Sheet
+?>
