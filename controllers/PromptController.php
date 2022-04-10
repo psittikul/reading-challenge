2302d5e55aca2631bccc691fcf90e92fa536bcc1
Patricia Sittikul <pas217@lehigh.edu>
2022-04-10 18:24:54 -0400

    details page

--- controllers/PromptController.php
+++ controllers/PromptController.php
@@ -16,13 +16,14 @@ class PromptController {
         echo " this ";
         $conn = $this->model->DB->conn;
         echo " thing ";
-        $prompts = $Prompt->all();
+        // $prompts = $Prompt->all();
         echo " still ";
         $users = [];
-        echo " on?\n";
+
         while ($row = $conn->query("SELECT * FROM users ORDER BY users.order DESC")->fetch_assoc()) {
             $users[] = $User->find($row['id']);
         }
+        echo " on?\n";
     }
 
 }
