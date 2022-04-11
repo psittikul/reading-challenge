b069463ddab3a469d0ecb2ec1c0163835779c591
Patricia Sittikul <pas217@lehigh.edu>
2022-04-11 19:14:32 -0400

    fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    // include 'includes/connection.php';
+    include 'models/User.php';
     include 'includes/header.php';
     include 'includes/_userRows.php';
     include 'includes/footer.php';
