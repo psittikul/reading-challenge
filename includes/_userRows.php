deeea577cfb8d9f6483dd4dcad37986bf643e0c6
Patricia Sittikul <pas217@lehigh.edu>
2022-04-11 18:33:48 -0400

    fix

--- includes/_userRows.php
+++ includes/_userRows.php
@@ -1,5 +1,17 @@
 <?php 
-    $query = "select y.freeReads, x.promptBooks, y.name, y.image_path, y.color, y.userID
+    $query = "select name,
+    freeReads,
+    promptBooks,
+    freeReads + 2*promptBooks as bookCount
+    from
+        (select
+        if(z.freeReads is null, 0, z.freeReads) as freeReads,
+        if(z.promptBooks is null, 0, z.promptBooks) as promptBooks,
+        z.name as name,
+        z.image_path as image_path,
+        z.color as color
+        from
+        (select y.freeReads as freeReads, x.promptBooks as promptBooks, y.name as name, y.image_path as image_path, y.color as color
         from
         (
             select users.id as userID, count(books.id) AS promptBooks
@@ -7,11 +19,11 @@
             where books.prompt_id > 0 AND books.status = 'Read' group by users.id
         ) as x
         right outer join (
-        select users.name, users.image_path, users.color, users.id as userID, count(books.id) as freeReads
+        select users.name, users.id as userID, image_path, color, count(books.id) as freeReads
         from users left outer join books on users.id = books.user_id
         where books.prompt_id is null AND books.status = 'Read' group by users.id
-        ) as y on x.userID = y.userID
-        order by bookCount DESC, y.name ASC;";
+        ) as y on x.userID = y.userID) as z) as az
+    order by bookCount desc, name asc;";
     $result = $conn->query($query);
     while($row = $result->fetch_assoc()) {
 ?>
