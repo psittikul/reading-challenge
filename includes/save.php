83f2029c88dd76026f4f6452e453c2351ded241c
Patricia Sittikul <pas217@lehigh.edu>
2022-04-12 16:23:48 -0400

    Fxi

--- includes/save.php
+++ includes/save.php
@@ -2,33 +2,32 @@
     $title = $_POST['title'];
     $author = $_POST['author'];
     $status = $_POST['status'];
-    $date_read = $_POST['date_read'];
+    $date_read = $_POST['date_read'] != '' ? $_POST['date_read'] : NULL;
     $user_id = $_POST['user_id'];
     $prompt_id = $_POST['prompt_id'] > 0 ? $_POST['prompt_id'] : NULL;
     $book_id = $_POST['book_id'];
-    $old_book_id = $_POST['old_book_id'];
+    $old_book_id = $_POST['old_book_id'] > 0 ? $_POST['old_book_id'] : NULL;
     var_dump($_POST);
-    // return;
 
-    // if($old_book_id > 0) {
-    //     $query = "UPDATE books SET prompt_id = NULL where id = $old_book_id";   
-    //     $result = $GLOBALS['conn']->query($query);
-    //     if(!$result) {
-    //         echo "Error: " . $GLOBALS['conn']->error;
-    //     }
-    // }
-    // if ($book_id > 0) {
-    //     $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
-    //         status = '$status', date_read = '$date_read' WHERE id = $book_id;";
-    // }
-    // else {
-    //     $query = "INSERT INTO books(id, title, author, status, date_read, user_id, prompt_id) VALUES(NULL, '$title', '$author', '$status', '$date_read', $user_id, $prompt_id);";
-    // }
+    if($old_book_id > 0) {
+        $query = "UPDATE books SET prompt_id = NULL where id = $old_book_id";   
+        $result = $GLOBALS['conn']->query($query);
+        if(!$result) {
+            echo "Error: " . $GLOBALS['conn']->error;
+        }
+    }
+    if ($book_id > 0) {
+        $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
+            status = '$status', date_read = '$date_read' WHERE id = $book_id;";
+    }
+    else {
+        $query = "INSERT INTO books(id, title, author, status, date_read, user_id, prompt_id) VALUES(NULL, '$title', '$author', '$status', '$date_read', $user_id, $prompt_id);";
+    }
 
     
-    // if($GLOBALS['conn']->query($query)) {
-    //     echo "SUCCESS?????????";
-    // }
-    // else {
-    //     echo "Error: " . $GLOBALS['conn']->error;
-    // }
+    if($GLOBALS['conn']->query($query)) {
+        echo "SUCCESS?????????";
+    }
+    else {
+        echo "Error: " . $GLOBALS['conn']->error;
+    }
