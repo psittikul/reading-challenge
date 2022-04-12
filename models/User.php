60839dec9c8d3a3d5f4bd9eb3cc2843cc259590c
Patricia Sittikul <pas217@lehigh.edu>
2022-04-12 14:26:44 -0400

    Fxi

--- models/User.php
+++ models/User.php
@@ -31,9 +31,8 @@ class User {
     }
     
     public function getQuarter($userID, $quarter) {
-        echo "get quarter stats for user: $userID quarter: $quarter\n";
+        // echo "get quarter stats for user: $userID quarter: $quarter\n";
         $data = [];
-        return $data;
         // $query = "select
         //     freeReads,
         //     promptBooks,
@@ -56,11 +55,7 @@ class User {
         //     where books.prompt_id is null AND books.status = 'Read' and user_id = $userID) as y on x.userID = y.userID) as z) as az;";
         $query_prompt = "SELECT count(books.id) as promptBooks from books partition($quarter) where user_id = $userID and status = 'Read' and prompt_id > 0";
         $query_free = "SELECT count(books.id) as freeReads from books partition($quarter) where user_id = $userID and status = 'Read' and prompt_id is null";
-        $data['query_prompt'] = $query_prompt;
-        $data['free'] = $query_free;
-        return $data;
         $result = $GLOBALS['conn']->query($query_prompt);
-        echo "Prompt books: " . $result->fetch_column(0) . "\n";
         $data['promptBooks'] = $result->fetch_column(0) ?? 0;
         
         $result = $GLOBALS['conn']->query($query_free);
