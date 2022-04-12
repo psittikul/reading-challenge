837eb77d465a6df35211ddac4122480305a75f1c
Patricia Sittikul <pas217@lehigh.edu>
2022-04-12 18:18:36 -0400

    Fxi

--- includes/save.php
+++ includes/save.php
@@ -9,14 +9,6 @@
     $prompt_id = $_POST['prompt_id'] == '' ? NULL : $_POST['prompt_id'];
     $book_id = $_POST['book_id'];
     $old_book_id = $_POST['old_book_id'] > 0 ? $_POST['old_book_id'] : NULL;
-    var_dump($prompt_id);
-    
-    $sql = "UPDATE BOOKS SET prompt_id = ?, title = ?, author = ?, status = ?, date_read = ? WHERE id = $book_id";
-    $stmt= $conn->prepare($sql);
-    $stmt->bind_param("sssss", $prompt_id, $title, $author, $status, $date_read);
-    $stmt->execute();
-
-    return; 
 
     if($old_book_id > 0) {
         $query = "UPDATE books SET prompt_id = NULL where id = $old_book_id";   
@@ -26,17 +18,25 @@
         }
     }
     if ($book_id > 0) {
-        if ($date_read != '') {
-            $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
-                status = '$status', date_read = '$date_read' WHERE id = $book_id;";
-        }
-        else {
-            $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
-                status = '$status', date_read = '0000-00-00' WHERE id = $book_id;";
-        }
+        $sql = "UPDATE BOOKS SET prompt_id = ?, title = ?, author = ?, status = ?, date_read = ? WHERE id = $book_id";
+        $stmt= $conn->prepare($sql);
+        $stmt->bind_param("sssss", $prompt_id, $title, $author, $status, $date_read);
+        $stmt->execute();
+        // if ($date_read != '') {
+        //     $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
+        //         status = '$status', date_read = '$date_read' WHERE id = $book_id;";
+        // }
+        // else {
+        //     $query = "UPDATE books SET prompt_id = $prompt_id, title = '$title', author = '$author', 
+        //         status = '$status', date_read = '0000-00-00' WHERE id = $book_id;";
+        // }
     }
     else {
-        $query = "INSERT INTO books(id, title, author, status, date_read, user_id, prompt_id) VALUES(NULL, '$title', '$author', '$status', '$date_read', $user_id, $prompt_id);";
+        // $query = "INSERT INTO books(id, title, author, status, date_read, user_id, prompt_id) VALUES(NULL, '$title', '$author', '$status', '$date_read', $user_id, $prompt_id);";
+        $sql = "INSERT INTO books(id, title, author, status, date_read, user_id, prompt_id) VALUES(NULL, ?, ?, ?, ?, ?, ?);";
+        $stmt= $conn->prepare($sql);
+        $stmt->bind_param("ssssss", $title, $author, $status, $date_read, $user_id, $prompt_id);
+        $stmt->execute();
     }
 
     echo "$query \n";
